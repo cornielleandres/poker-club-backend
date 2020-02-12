d2c618bc5ee38c6ed87b88c4cfe42451560a1405
Carlos Vargas <cornielleandres@gmail.com>
2020-02-12 07:18:04 -0500

    Update getDefaultAvatarById function.

--- data/models/defaultAvatarsDb.js
+++ data/models/defaultAvatarsDb.js
@@ -1,6 +1,10 @@
 const db	= require('../dbConfig.js');
 
 module.exports = {
-	getDefaultAvatarById: id => db('default-avatars').where({ id }).select('default_avatar').first(),
+	getDefaultAvatarById: async id => {
+		const defaultAvatar = await db('default-avatars').where({ id }).select('default_avatar').first();
+		if (!defaultAvatar) throw new Error(`No default avatar found with id ${ id }`);
+		return defaultAvatar.default_avatar.toString();
+	},
 	getDefaultAvatars: () => db('default-avatars').select('id', 'default_avatar'),
 };
