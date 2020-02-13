206881f0b19d9a164d3ba8dcab877bb631ca27a8
Carlos Vargas <cornielleandres@gmail.com>
2020-02-13 06:39:01 -0500

    Update how player chat payloads are handled.

--- utils/events/table/handleAddToPlayerChat.js
+++ utils/events/table/handleAddToPlayerChat.js
@@ -17,7 +17,8 @@ module.exports = async (io, socket, message) => {
 		const { handleTablePlayerPayloads }	= require('../../index.js');
 		const { user_id } = socket;
 		const { table_id } = await tablePlayerDb.getTablePlayerByUserId(user_id);
-		return handleTablePlayerPayloads(io, table_id, 'update_player_chat', null, null, { message, user_id });
+		const playerChatPayload = { type: 'player_chat', payload: { message, user_ids: [ user_id ] } };
+		return handleTablePlayerPayloads(io, table_id, 'update_player_chat', null, null, playerChatPayload);
 	} catch (e) {
 		const errMsg = 'Player Chat Error: ' + e.toString();
 		console.log(errMsg);
