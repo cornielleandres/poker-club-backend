95ec730a316df812efc615f26e46df4a7579381c
Carlos Vargas <cornielleandres@gmail.com>
2020-03-09 06:00:09 -0400

    Only send payload if player is sitting at table.

--- utils/events/table/handleTablePlayerPayloads.js
+++ utils/events/table/handleTablePlayerPayloads.js
@@ -19,68 +19,80 @@ module.exports = async (io, table_id, event_name, positions, delayTime, chatMess
 	const { delay, handleError, isNonEmptyObject }	= require('../../index.js');
 	try {
 		const table = await tableDb.getTable(table_id);
-		const user_ids = table.players.map(p => p && p.user_id).filter(p => p);
+		const tablePlayers = table.players;
+		const user_ids = tablePlayers.map(p => p && p.user_id).filter(p => p);
 		const allPlayerNotes = await playerNotesDb.getAllPlayerNotes(user_ids);
 		io.in(table_room + table_id).clients((err, clients) => {
 			try {
-				if (err) throw new Error(err);
+				if (err) throw err;
 				const clientsAndPayloads = [];
 				clients.forEach(client => {
 					const clientSocket = io.sockets.connected[ client ];
 					if (clientSocket) { // if client is still connected
 						const { user_id: clientUserId } = clientSocket;
-						if (chatMessagePayload) { // if sending a chat message
-							let payload = chatMessagePayload;
-							if (chatMessagePayload.payload.user_ids) {
-								payload = _.cloneDeep(chatMessagePayload);
-								payload.payload.playerNames = payload.payload.user_ids.map(user_id => {
-									if (user_id === clientUserId) return 0; // 0 is used to indicate the client itself
-									const player = table.players.find(p => p && p.user_id === user_id);
-									if (!player) return null;
-									return player.name;
-								});
-								delete payload.payload.user_ids;
+						const playerIndex = tablePlayers.findIndex(p => p && p.user_id === clientUserId);
+						if (playerIndex !== -1) { // if player is still sitting at table
+							if (chatMessagePayload) { // if sending a chat message
+								let payload = chatMessagePayload;
+								if (chatMessagePayload.payload.user_ids) {
+									payload = _.cloneDeep(chatMessagePayload);
+									payload.payload.playerNames = payload.payload.user_ids.map(user_id => {
+										if (user_id === clientUserId) return 0; // 0 is used to indicate the client itself
+										const player = table.players.find(p => p && p.user_id === user_id);
+										if (!player) return null;
+										return player.name;
+									});
+									delete payload.payload.user_ids;
+								}
+								return clientsAndPayloads.push({ client, payload });
 							}
-							return clientsAndPayloads.push({ client, payload });
+							const clientTable = _.cloneDeep(table);
+							const { players } = clientTable;
+							clientTable.players = players
+								.slice(playerIndex)
+								.concat(players.slice(0, playerIndex))
+								.map(p => {
+									// if the following conditions are met, hide the player's cards in the payload
+									if (
+										p // is a player
+										&& p.user_id !== clientUserId // is not the client player
+										&& p.cards.length // has not folded
+										&& isNonEmptyObject(p.cards[0]) // is currently in the hand
+										&& p.hide_cards // is set to hide their cards
+									) p.cards = p.cards.map(() => ({ rank: 0 })); // map to hidden cards
+									return p;
+								});
+							const playerNotes = allPlayerNotes.filter(notes => notes.user_id === clientUserId);
+							if (playerNotes.length) {
+								playerNotes.forEach(notes => {
+									const player = players.find(p => p && p.user_id === notes.player_notes_user_id);
+									player.notes = notes.notes;
+								});
+							}
+							const payload = { positions, table: clientTable };
+							clientsAndPayloads.push({ client, payload });
 						}
-						const clientTable = _.cloneDeep(table);
-						const { players } = clientTable;
-						const playerIndex = players.findIndex(p => p && p.user_id === clientUserId);
-						if (playerIndex === -1) throw new Error('Could not find client player in table.');
-						clientTable.players = players
-							.slice(playerIndex)
-							.concat(players.slice(0, playerIndex))
-							.map(p => {
-								// if the following conditions are met, hide the player's cards in the payload
-								if (
-									p // is a player
-									&& p.user_id !== clientUserId // is not the client player
-									&& p.cards.length // has not folded
-									&& isNonEmptyObject(p.cards[0]) // is currently in the hand
-									&& p.hide_cards // is set to hide their cards
-								) p.cards = p.cards.map(() => ({ rank: 0 })); // map to hidden cards
-								return p;
-							});
-						const playerNotes = allPlayerNotes.filter(notes => notes.user_id === clientUserId);
-						if (playerNotes.length) {
-							playerNotes.forEach(notes => {
-								const player = players.find(p => p && p.user_id === notes.player_notes_user_id);
-								player.notes = notes.notes;
-							});
-						}
-						const payload = { positions, table: clientTable };
-						clientsAndPayloads.push({ client, payload });
 					}
 				});
 				clientsAndPayloads.forEach(c => io.to(c.client).emit(event_name, c.payload));
 			} catch (e) {
-				const err = `Error for event "${ event_name }": ${ e }`;
-				return handleError('Error sending data to table players.', err, null, io, table_room + table_id);
+				return handleError(
+					`Error sending data to table players for event "${ event_name }".`,
+					e,
+					null,
+					io,
+					table_room + table_id,
+				);
 			}
 		});
 		if (delayTime) return delay(delayTime);
 	} catch (e) {
-		const err = `Error for event "${ event_name }": ${ e }`;
-		return handleError('Error getting table player clients.', err, null, io, table_room + table_id);
+		return handleError(
+			`Error getting table player clients for event "${ event_name }".`,
+			e,
+			null,
+			io,
+			table_room + table_id,
+		);
 	}
 };
