e8799628d403c10427d774a6d7e660492704b5ed
Carlos Vargas <cornielleandres@gmail.com>
2020-03-19 12:16:59 -0400

    Uncomment out code.

--- utils/events/table/handlePlayerTimer.js
+++ utils/events/table/handlePlayerTimer.js
@@ -30,23 +30,23 @@ module.exports = async (io, table_id, table_type, action, street, hand_id) => {
 	newTimerEnd.setMilliseconds(newTimerEnd.getMilliseconds() + total_time);
 	await tablePlayerDb.updateTimerEnd(table_id, action, newTimerEnd);
 	await handleTablePlayerPayloads(io, table_id, 'update_player_timer', [ action ]);
-	// return setTimeout(async () => {
-	// 	try {
-	// 		const currentTable = await tableDb.getStreetAndHandId(table_id);
-	// 		const { bet, position, user_id } = await tablePlayerDb.getActionPlayerByTableId(table_id);
-	// 		// if these conditions are satisfied, it means player did not act in time
-	// 		if (
-	// 			action === position
-	// 			&& street === currentTable.street
-	// 			&& hand_id === currentTable.hand_id
-	// 		) {
-	// 			const redisClientKey = usersKey + user_id;
-	// 			const playerSocketId = await redisClient.getAsync(redisClientKey);
-	// 			const playerSocket = io.sockets.connected[ playerSocketId ];
-	// 			return handleDefaultAction(io, playerSocket, table_id, user_id, bet);
-	// 		}
-	// 	} catch (e) {
-	// 		return handleError('Error handling player timer.', e, null, io, table_room + table_id);
-	// 	}
-	// }, total_time);
+	return setTimeout(async () => {
+		try {
+			const currentTable = await tableDb.getStreetAndHandId(table_id);
+			const { bet, position, user_id } = await tablePlayerDb.getActionPlayerByTableId(table_id);
+			// if these conditions are satisfied, it means player did not act in time
+			if (
+				action === position
+				&& street === currentTable.street
+				&& hand_id === currentTable.hand_id
+			) {
+				const redisClientKey = usersKey + user_id;
+				const playerSocketId = await redisClient.getAsync(redisClientKey);
+				const playerSocket = io.sockets.connected[ playerSocketId ];
+				return handleDefaultAction(io, playerSocket, table_id, user_id, bet);
+			}
+		} catch (e) {
+			return handleError('Error handling player timer.', e, null, io, table_room + table_id);
+		}
+	}, total_time);
 };
