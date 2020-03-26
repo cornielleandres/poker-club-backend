4ca2104043c0f39cae32530ddfaf8753d4add763
Carlos Vargas <cornielleandres@gmail.com>
2020-03-26 12:09:33 -0400

    Remove updating hand descriptions and async/await.

--- utils/events/table/player-hands/getWinners.js
+++ utils/events/table/player-hands/getWinners.js
@@ -7,13 +7,12 @@ const {
 	maxPlayers,
 }	= constants;
 
-module.exports = async (io, table_id, players, community_cards, pot, game_type) => {
+module.exports = (players, community_cards, pot, game_type) => {
 	const { isNonEmptyObject }	= require('../../../index.js');
-	const { getPlayerHands, handlePossibleSplitPot, updateHandDescriptions }	= require('./index.js');
+	const { getPlayerHands, handlePossibleSplitPot }	= require('./index.js');
 	const defaultMaxPlayers = maxPlayers[0];
 	const playersWithCards = players.filter(p => p && p.cards.length && isNonEmptyObject(p.cards[0]));
 	getPlayerHands(playersWithCards, community_cards, game_type);
-	await updateHandDescriptions(io, table_id, playersWithCards);
 	const winners = [];
 	pot.forEach(sidePot => {
 		const potPlayers = playersWithCards.filter(p => sidePot.user_ids.includes(p.user_id));
